63c40ddaa602ef32bc8c5a4c91b936836ed58dc6
James Berck III <jwberck1@gmail.com>
2016-11-23 23:32:25 -0600

    initial commit

---
+++ Assets/ballScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballScript : MonoBehaviour {
+
+	public GameObject plane;
+
+	public GameObject SpawnPoint;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (transform.position.y < (plane.transform.position.y - 10)) {
+			transform.position = SpawnPoint.transform.position;
+		}
+
+	}
+}
